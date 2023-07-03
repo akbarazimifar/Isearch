449c5afc31a4cb9418969089d5281e577d0dbde1
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-03 11:14:35 +0200

    Add files via upload

---
+++ lib/all_user_taged.py
@@ -0,0 +1,49 @@
+from lib.requests import *
+from lib.api import api_instagram
+from lib.objects import TempPrint
+import json
+
+class Finder:
+    def __init__(self, data, headers, cookies):
+        self.data = data
+        self.headers = headers
+        self.cookies = cookies
+
+    async def extract_users(self):
+            usernames_tag = []
+            
+            utimeline_media = self.data["graphql"]["user"]["edge_owner_to_timeline_media"]
+
+            for edge in utimeline_media["edges"]:
+                tagged_users = edge["node"]["edge_media_to_tagged_user"]["edges"]
+                
+                for tagged_user in tagged_users:
+                    username = tagged_user["node"]["user"]["username"]
+
+                    if username != self.data['graphql']['user']['username']:
+                        usernames_tag.append(username)
+        
+            return usernames_tag
+
+    async def account_to_id(self):
+        founds = 0
+        users = await self.extract_users()
+
+        print()
+        TempPrint("-> 🐅 Scraping of all tagged profiles...").TPrint()
+        try:
+            for user in users:
+                founds += 1
+                r = await Requests(api_instagram(user), headers=self.headers, cookies=self.cookies).sender()
+                data = json.loads(r.text)
+
+                user_id = data['logging_page_id'].strip('profilePage_')
+
+
+                print(f"\r- Name: @{user}\n   ╚═════ID : {user_id}\n")
+
+            if founds == 0:
+                print("[-] The target has not tagged a profile.")
+
+        except:
+            pass
