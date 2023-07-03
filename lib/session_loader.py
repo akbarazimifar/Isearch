8d96907e07c625a5c697229d29a0b7660428e138
Norze <123885505+N0rz3@users.noreply.github.com>
2023-07-03 18:21:03 +0200

    Update session_loader.py

--- lib/session_loader.py
+++ lib/session_loader.py
@@ -1,5 +1,8 @@
 import json
 from lib.objects import TempPrint
+from lib.colors import GREEN, WHITE, RED
+from lib.banner import b1
+
 
 async def launch():
     TempPrint("[+] 🤔 Session ID checking...").TPrint()
@@ -19,3 +22,22 @@ async def launch():
             TempPrint("[+] 👍 Session ID found.").TPrint()
             print()
             return session_id
+
+
+async def checker():
+    print(b1)
+
+    TempPrint("[+] 🤔 Session ID checking...").TPrint()
+    with open("data.json", "r") as file:
+        read = json.load(file)
+
+        sessionID = read['sessionID']
+
+    if sessionID != "YOUR_SESSION_ID":
+        print(GREEN + "[+] Session ID found! You can use the whole tool." + WHITE)
+
+    elif sessionID != "":
+        print(GREEN + "[+] Session ID found! You can use the whole tool." + WHITE)
+
+    else:
+        print(RED + "[-] Session ID not found, drop it in the data.json file." + WHITE)
